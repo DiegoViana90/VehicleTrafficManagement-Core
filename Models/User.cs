0385f4eca244e9fdcb02c213aa5fb6a4985106b1
Diego <dtviana90@gmail.com>
2024-07-09 16:50:44 -0300

    feat: ajusta model de User

--- Models/User.cs
+++ Models/User.cs
@@ -6,8 +6,10 @@ namespace VehicleTrafficManagement.Models;
 public class User : IdentityUser
 {
     public string FullName { get; set; }
-    public DateTime DateOfBirth { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
     public UserType UserType { get; set; } 
+    public bool IsFirstAcess { get; set; } = true;
     public int CompanyId { get; set; }
     public Company Company { get; set; }
 }
