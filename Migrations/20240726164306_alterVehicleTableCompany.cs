980765b747af047e9bbab4c20882ffaea8fd433c
Diego <dtviana90@gmail.com>
2024-07-26 15:37:15 -0300

    refactor: ajusta veículos para company

---
+++ Migrations/20240726164306_alterVehicleTableCompany.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VehicleTrafficManagement.Migrations
+{
+    public partial class alterVehicleTableCompany : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CompaniesId",
+                table: "Vehicles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicles_CompaniesId",
+                table: "Vehicles",
+                column: "CompaniesId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vehicles_Companies_CompaniesId",
+                table: "Vehicles",
+                column: "CompaniesId",
+                principalTable: "Companies",
+                principalColumn: "CompaniesId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vehicles_Companies_CompaniesId",
+                table: "Vehicles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Vehicles_CompaniesId",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "CompaniesId",
+                table: "Vehicles");
+        }
+    }
+}
